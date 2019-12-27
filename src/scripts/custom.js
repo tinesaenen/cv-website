ef58a1efc7132c3f37297301b5980aebf766e173
tine <tinesaenen@hotmail.be>
2019-12-27 10:00:24 +0100

    eerste commit, uploaden bestaande site

---
+++ src/scripts/custom.js
@@ -0,0 +1,23 @@
+// om naar boven te scrollen met knop
+
+mybutton = document.getElementById("toTopButton");
+mybutton.addEventListener("click", function() {
+  topFunction();
+});
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
